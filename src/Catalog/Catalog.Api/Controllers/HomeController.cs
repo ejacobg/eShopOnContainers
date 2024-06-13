ceb78f88e08bd5acb3143ec983cb7c80dd5410e8
Jacob Gayban <ejacobg@outlook.com>
2024-06-12 19:25:46 -0700

    Add HomeController

---
+++ src/Catalog/Catalog.Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+public class HomeController : Controller
+{
+    // GET: /<controller>/
+    public IActionResult Index()
+    {
+        return new RedirectResult("~/swagger");
+    }
+}
