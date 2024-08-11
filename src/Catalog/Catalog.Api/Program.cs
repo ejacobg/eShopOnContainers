206c1aeee0dd08534864406afb14699c50165216
Jacob Gayban <ejacobg@outlook.com>
2024-08-10 17:06:06 -0700

    Add Program.cs to Catalog
    currently not working

--- src/Catalog/Catalog.Api/Program.cs
+++ src/Catalog/Catalog.Api/Program.cs
@@ -1,44 +1,125 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Net;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+using Azure.Core;
+using Azure.Identity;
 
-var app = builder.Build();
+using Catalog.Api;
+using Catalog.Api.Extensions;
+using Catalog.Api.Infrastructure;
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using IntegrationEventLog;
+
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Options;
+
+using Serilog;
+
+var configuration = GetConfiguration();
+
+Log.Logger = CreateSerilogLogger(configuration);
+
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Configuring web host ({ApplicationContext})...", Program.AppName);
+    var host = CreateHostBuilder(configuration, args);
+
+    Log.Information("Applying migrations ({ApplicationContext})...", Program.AppName);
+    host.MigrateDbContext<CatalogContext>((context, services) =>
+    {
+        var env = services.GetService<IWebHostEnvironment>();
+        var settings = services.GetService<IOptions<CatalogSettings>>();
+        var logger = services.GetService<ILogger<CatalogContextSeed>>();
+
+        new CatalogContextSeed().SeedAsync(context, env, settings, logger).Wait();
+    })
+    .MigrateDbContext<IntegrationEventLogContext>((_, __) => { });
+
+    Log.Information("Starting web host ({ApplicationContext})...", Program.AppName);
+    host.Run();
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", Program.AppName);
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+IWebHost CreateHostBuilder(IConfiguration configuration, string[] args)
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddSerilog();
+    return builder.WebHost
+        .UseConfiguration(configuration)
+        .CaptureStartupErrors(false)
+        .ConfigureKestrel(options =>
+        {
+            var ports = GetDefinedPorts(configuration);
+            options.Listen(IPAddress.Any, ports.httpPort, listenOptions =>
+            {
+                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+            });
+            options.Listen(IPAddress.Any, ports.grpcPort, listenOptions =>
+            {
+                listenOptions.Protocols = HttpProtocols.Http2;
+            });
 
-app.MapGet("/weatherforecast", () =>
+        })
+        .UseStartup<Startup>()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseWebRoot("Pics")
+        .Build();
+}
+
+Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var seqServerUrl = configuration["Serilog:SeqServerUrl"];
+    var logstashUrl = configuration["Serilog:LogstashgUrl"];
+    return new LoggerConfiguration()
+        .MinimumLevel.Verbose()
+        .Enrich.WithProperty("ApplicationContext", Program.AppName)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "http://seq" : seqServerUrl)
+        .WriteTo.Http(string.IsNullOrWhiteSpace(logstashUrl) ? "http://logstash:8080" : logstashUrl, null)
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+(int httpPort, int grpcPort) GetDefinedPorts(IConfiguration config)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    var grpcPort = config.GetValue("GRPC_PORT", 81);
+    var port = config.GetValue("PORT", 80);
+    return (port, grpcPort);
+}
+
+IConfiguration GetConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+    var config = builder.Build();
+
+    if (config.GetValue<bool>("UseVault", false))
+    {
+        TokenCredential credential = new ClientSecretCredential(
+            config["Vault:TenantId"],
+            config["Vault:ClientId"],
+            config["Vault:ClientSecret"]);
+        //builder.AddAzureKeyVault(new Uri($"https://{config["Vault:Name"]}.vault.azure.net/"), credential);        
+    }
+
+    return builder.Build();
+}
+
+public partial class Program
+{
+    public static string Namespace = typeof(Startup).Namespace;
+    public static string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
 }
