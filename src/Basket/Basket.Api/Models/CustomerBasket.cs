84c08b738b25111a32d8bfc2fe16743dd8f9ffbb
Jacob Gayban <ejacobg@outlook.com>
2025-01-30 09:47:56 -0800

    Add Basket API models

---
+++ src/Basket/Basket.Api/Models/CustomerBasket.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.Api.Models;
+
+public class CustomerBasket
+{
+    public string BuyerId { get; set; }
+
+    public List<BasketItem> Items { get; set; } = new();
+
+    public CustomerBasket()
+    {
+
+    }
+
+    public CustomerBasket(string customerId)
+    {
+        BuyerId = customerId;
+    }
+}
---
+++ src/Basket/Basket.Api/Models/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.Api.Models;
+
+public interface IBasketRepository
+{
+    Task<CustomerBasket> GetBasketAsync(string customerId);
+
+    // TODO GetUsersAsync()?
+    IEnumerable<string> GetUsers();
+    Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+    Task<bool> DeleteBasketAsync(string id);
+}
